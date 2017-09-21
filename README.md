9b5c94c64a779013e5b21253238a954bcab536fc
Sg4Dylan <i@acg.works>
2017-09-22 00:22:06 +0800

    move to python3 & add absolute path support

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # NeteaseCloudMusicPlaylistCreator
 这个项目主要是在Windows上创建网易云音乐的播放列表(格式m3u)，方便导入MP3
-##使用方法：
+
+## 使用方法：
 1. 安装网易云音乐客户端
-2. 下载音乐
-3. 将脚本放在网易云音乐的音乐下载目录并运行
-4. 将音乐文件和生成的m3u文件拷贝到你的播放器目录（前提是要你的播放器支持m3u例如Sony的NW-M505
+2. 登录账户，下载播放列表中所有音乐
+3. 双击运行脚本
+4. 将音乐文件和生成的m3u文件拷贝到你的播放器目录（前提是要你的播放器支持 m3u
+
+## Tips：
+如果你只需要在电脑中其他播放器使用 M3U 文件，可以使用绝对路径生成模式
+将参数 `-a` 设置为真，然后直接在下载目录运行脚本
+例如 -> `create_playlist.py -a True`
+也可以使用 `-p` 参数手动传递网易云音乐的歌曲下载目录位置
+例如 -> `create_playlist.py -a True -p "D:\NeteaseCloudMusicDownload"`
+
+## 注意：
+本 fork 为 Python3+ only，Python2 麻烦绕道原 repo
--- create_playlist.py
+++ create_playlist.py
@@ -2,69 +2,88 @@ import sqlite3
 import json
 import codecs
 import os
+import argparse
 
-cx = sqlite3.connect(os.path.expanduser('~')+"/AppData/Local/Netease/CloudMusic/Library/webdb.dat")
+cx = sqlite3.connect(
+    os.path.expanduser('~') + \
+    "/AppData/Local/Netease/CloudMusic/Library/webdb.dat"
+    )
 cx.row_factory = sqlite3.Row
 
 def getPlaylist():
-	cu=cx.cursor()
-	cu.execute("select * from web_playlist") 
-	playlists=[]
-	for item in cu.fetchall():
-		playlist=(item["pid"],getPlaylistNameFromJson(item["playlist"]))
-		playlists.append(playlist)
-	return playlists
+    cu=cx.cursor()
+    cu.execute("select * from web_playlist") 
+    playlists=[]
+    for item in cu.fetchall():
+        playlist=(item["pid"],getPlaylistNameFromJson(item["playlist"]))
+        playlists.append(playlist)
+    return playlists
 
 def getPlayListMusic(pid):
-	cu=cx.cursor()
-	cu.execute("select * from web_playlist_track where pid=?",[pid]) 
-	musics=[]
-	for item in cu.fetchall():
-		musics.append(item["tid"]);
-	return musics
+    cu=cx.cursor()
+    cu.execute("select * from web_playlist_track where pid=?",[pid]) 
+    musics=[]
+    for item in cu.fetchall():
+        musics.append(item["tid"]);
+    return musics
 
-def getOfflineMusicDetail(tid):
-	cu=cx.cursor()
-	cu.execute("select * from web_offline_track where track_id=?",[tid]) 
-	music = cu.fetchone()
-	if music is None:
-		return None
-	detail = (getMusicNameFromJson(music["detail"]), music["relative_path"])
-	return detail
+def getOfflineMusicDetail(tid, absPath=None):
+    cu=cx.cursor()
+    cu.execute("select * from web_offline_track where track_id=?",[tid]) 
+    music = cu.fetchone()
+    if music is None:
+        return None
+    fullPath = music["relative_path"]
+    if absPath:
+        fullPath = absPath + fullPath
+    detail = (getMusicNameFromJson(music["detail"]), fullPath)
+    return detail
 
-def writePlaylistToFile(pid, playlistName):
-	file = codecs.open(playlistName + ".m3u", "w", "utf-8")
-	count = 0
-	try:
-		file.writelines("#EXTM3U")
-		musicIds = getPlayListMusic(pid)
-		for tid in musicIds:
-			if tid is not None:
-				detail=getOfflineMusicDetail(tid)
-				if detail is not None:
-					count=count + 1
-					file.writelines(u"\n#EXTINF:" + detail[0] + u"\n" + detail[1])
-	except Exception, e:
-		raise
-	else:
-		pass
-	finally:
-		file.close()
-		if count <= 0:
-			os.remove(playlistName + ".m3u")
+def writePlaylistToFile(pid, playlistName, absPath=None):
+    file = codecs.open(playlistName + ".m3u", "w", "utf-8")
+    count = 0
+    try:
+        file.writelines("#EXTM3U")
+        musicIds = getPlayListMusic(pid)
+        for tid in musicIds:
+            if tid is not None:
+                detail=getOfflineMusicDetail(tid, absPath)
+                if detail is not None:
+                    count=count + 1
+                    file.writelines("\n#EXTINF:" + detail[0] + "\n" + detail[1])
+    except Exception as e:
+        raise
+    else:
+        pass
+    finally:
+        file.close()
+        if count <= 0:
+            os.remove(playlistName + ".m3u")
 
 def getPlaylistNameFromJson(jsonStr):
-	playlistDetail = json.loads(jsonStr)
-	return playlistDetail["name"].encode("GBK", 'ignore');
+    playlistDetail = json.loads(jsonStr)
+    return playlistDetail["name"]
 
 def getMusicNameFromJson(jsonStr):
-	musicDetail = json.loads(jsonStr)
-	return musicDetail["name"];
+    musicDetail = json.loads(jsonStr)
+    return musicDetail["name"]
 
-def main():
-	playlists = getPlaylist()
-	for item in playlists:
-		writePlaylistToFile(item[0], item[1])
+def main(absPath=None):
+    playlists = getPlaylist()
+    for item in playlists:
+        writePlaylistToFile(item[0], item[1], absPath)
 
 if __name__ == '__main__':
-	main()
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
+                                     description='NeteaseCloudMusicPlaylistConvert')
+    parser.add_argument('-a', '--abs', help='M2U 中使用绝对路径')
+    parser.add_argument('-p', '--path', help='网易云音乐下载目录绝对路径')
+    useAbsPath = parser.parse_args().abs
+    absPath = parser.parse_args().path
+    if not absPath:
+        absPath = os.getcwd()
+    if not absPath.endswith('\\'):
+        absPath += '\\'
+    if str(useAbsPath).upper() != "TRUE":
+        absPath = None
+    main(absPath)
